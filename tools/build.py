bfc46fcbb10eb895a9b0bb9df27150f32037882e
Khaled Hosny <khaledhosny@eglug.org>
2013-06-23 16:38:44 +0200

    Arrange sources

--- tools/build.py
+++ tools/build.py
@@ -6,12 +6,12 @@ font = fontforge.open(sys.argv[1])
 if len(sys.argv) > 4:
   font.mergeFeature(sys.argv[4])
 
-font.mergeFonts("it.sfd")
-font.mergeFonts("bf.sfd")
-font.mergeFonts("bi.sfd")
-font.mergeFonts("sfup.sfd")
-font.mergeFonts("sfit.sfd")
-font.mergeFonts("sfbf.sfd")
+font.mergeFonts("sources/it.sfd")
+font.mergeFonts("sources/bf.sfd")
+font.mergeFonts("sources/bi.sfd")
+font.mergeFonts("sources/sfup.sfd")
+font.mergeFonts("sources/sfit.sfd")
+font.mergeFonts("sources/sfbf.sfd")
 
 font.version = sys.argv[3]
 font.generate(sys.argv[2], flags=("round", "opentype"))
