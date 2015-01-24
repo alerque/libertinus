13a8efb4636744871490061cc2f652c795ab7de8
Khaled Hosny <khaledhosny@eglug.org>
2015-01-24 13:15:15 +0200

    Ensure copyright text is consistent and up to date
    Put it in a central place and set it at build time.

--- tools/build.py
+++ tools/build.py
@@ -6,6 +6,7 @@ parser.add_argument("-i", "--input", required=True)
 parser.add_argument("-o", "--output", required=True)
 parser.add_argument("-v", "--version", required=True)
 parser.add_argument("-f", "--feature-file", required=False)
+parser.add_argument("-c", "--copyright-file", required=True, type=argparse.FileType('r'))
 
 args = parser.parse_args()
 
@@ -14,7 +15,10 @@ font = fontforge.open(args.input)
 if args.feature_file:
     font.mergeFeature(args.feature_file)
 
+copyright = args.copyright_file.read()
+
 font.version = args.version
+font.copyright = copyright.rstrip() % font.fullname
 font.selection.all()
 font.autoHint()
 font.generate(args.output, flags=("opentype"))
