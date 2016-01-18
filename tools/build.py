87ef45c8034a589e9f2610411ce2e2e006e278b2
Khaled Hosny <khaledhosny@eglug.org>
2016-01-18 22:47:18 +0400

    Update font files metadata
    * Fix copyright
    * Set proper vendor
    * Use only OFL

--- tools/build.py
+++ tools/build.py
@@ -19,7 +19,19 @@ if args.feature_file:
 copyright = args.copyright_file.read()
 
 font.version = args.version
-font.copyright = copyright.rstrip() % date.today().year
+font.copyright = "Copyright 2003-2012 by Philipp H. Poll, copyright 2012-%s Khaled Hosny." % date.today().year
+
+font.appendSFNTName("English (US)", "Manufacturer", "Khaled Hosny")
+font.appendSFNTName("English (US)", "Vendor URL", "https://github.com/khaledhosny/libertine")
+font.appendSFNTName("English (US)", "License URL", "http://scripts.sil.org/OFL")
+font.appendSFNTName("English (US)", "License", '\
+This Font Software is licensed under the SIL Open Font License, Version 1.1. \
+This Font Software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR \
+CONDITIONS OF ANY KIND, either express or implied. See the SIL Open Font License \
+for the specific language, permissions and limitations governing your use of \
+this Font Software.')
+font.os2_vendor = "BLQ "
+
 font.selection.all()
 font.autoHint()
 font.generate(args.output, flags=("opentype"))
