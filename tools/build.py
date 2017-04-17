0d3438fa806f4f236cf3b77d968281b64a992493
Khaled Hosny <khaledhosny@eglug.org>
2017-04-17 02:34:19 +0200

    Cleanup copyrights
    Add AUTHORS.txt and CONTRIBUTORS.txt files listing all authors
    (including STIX ones that were missing before) and simplify the
    copyright and license text.
    
    Oh, and I hate “Reserved Font Name”, really hate it!

--- tools/build.py
+++ tools/build.py
@@ -1,3 +1,4 @@
+# encoding: utf-8
 import sys
 try:
     from sortsmill import ffcompat as fontforge
@@ -22,17 +23,12 @@ if args.feature_file and path.isfile(args.feature_file):
     font.mergeFeature(args.feature_file)
 
 font.version = args.version
-font.copyright = "Copyright 2003-2012 by Philipp H. Poll. Copyright 2012-%s Khaled Hosny." % date.today().year
+font.copyright = u"Copyright © 2012-%s The Libertinus Project Authors." % date.today().year
 
 font.appendSFNTName("English (US)", "Manufacturer", "Khaled Hosny")
 font.appendSFNTName("English (US)", "Vendor URL", "https://github.com/khaledhosny/libertinus")
 font.appendSFNTName("English (US)", "License URL", "http://scripts.sil.org/OFL")
-font.appendSFNTName("English (US)", "License", '\
-This Font Software is licensed under the SIL Open Font License, Version 1.1. \
-This Font Software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR \
-CONDITIONS OF ANY KIND, either express or implied. See the SIL Open Font License \
-for the specific language, permissions and limitations governing your use of \
-this Font Software.')
+font.appendSFNTName("English (US)", "License", 'This Font Software is licensed under the SIL Open Font License, Version 1.1.')
 font.os2_vendor = "BLQ "
 
 font.selection.all()
