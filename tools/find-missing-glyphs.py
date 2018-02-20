3cb5652f9f4fdb82d871926311d9228e6fbf4ca9
Khaled Hosny <khaledhosny@eglug.org>
2018-02-20 21:37:35 +0200

    Python 3 and FontForge compatibility

--- tools/find-missing-glyphs.py
+++ tools/find-missing-glyphs.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+
 import fontforge
 import sys
 
@@ -15,5 +17,5 @@ for lookup in font.gsub_lookups + font.gpos_lookups:
                 for possub in glyph.getPosSub(subtable):
                     missing.update([n for n in possub[2:] if n not in font])
 if missing:
-    print "Font is missing: %s" % " ".join([n for n in missing])
+    print("Font is missing: %s" % " ".join([n for n in missing]))
     sys.exit(1)
