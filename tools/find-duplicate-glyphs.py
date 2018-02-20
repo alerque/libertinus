5363ac9fb35a1c65813478290c33da64097bb46a
Khaled Hosny <khaledhosny@eglug.org>
2018-02-20 21:37:35 +0200

    Python 3 and FontForge compatibility

--- tools/find-duplicate-glyphs.py
+++ tools/find-duplicate-glyphs.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+
 import fontforge
 import sys
 
@@ -5,12 +7,13 @@ font = fontforge.open(sys.argv[1])
 glyphs = {}
 
 for glyph in font.glyphs():
-    if glyph.name not in glyphs:
-        glyphs[glyph.name] = 0
-    glyphs[glyph.name] += 1
+    name = glyph.glyphname
+    if name not in glyphs:
+        glyphs[name] = 0
+    glyphs[name] += 1
 
 duplicates = [g for g in glyphs if glyphs[g] > 1]
 
 if duplicates:
-    print "Font has duplicate glyphs: %s" % " ".join(duplicates)
+    print("Font has duplicate glyphs: %s" % " ".join(duplicates))
     sys.exit(1)
