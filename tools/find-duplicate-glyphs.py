0fb1066e935faff7c0696b37f9526f8164d67878
Khaled Hosny <khaledhosny@eglug.org>
2018-03-09 18:08:16 +0200

    Don’t run tests when source didn’t change

--- tools/find-duplicate-glyphs.py
+++ tools/find-duplicate-glyphs.py
@@ -14,6 +14,11 @@ for glyph in font.glyphs():
 
 duplicates = [g for g in glyphs if glyphs[g] > 1]
 
-if duplicates:
-    print("Font has duplicate glyphs: %s" % " ".join(duplicates))
-    sys.exit(1)
+with open(sys.argv[2], "w") as logfile:
+    msg = "PASS"
+    if duplicates:
+        msg = "FAIL: Font has duplicate glyphs: %s" % " ".join(duplicates)
+        print(msg)
+    print(msg, file=logfile)
+
+sys.exit(len(duplicates))
