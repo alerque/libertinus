d528329d6926d831579642c17440afd7ef4f0c59
Khaled Hosny <khaledhosny@eglug.org>
2018-04-01 21:29:37 +0200

    Merge two scripts

--- tools/check-errors.py
+++ tools/check-errors.py
@@ -4,8 +4,10 @@ import fontforge
 import sys
 
 font = fontforge.open(sys.argv[1])
-missing = set()
+names = [g.glyphname for g in font.glyphs()]
+duplicates = set(n for n in names if names.count(n) > 1)
 
+missing = set()
 for lookup in font.gsub_lookups + font.gpos_lookups:
     kind = font.getLookupInfo(lookup)[0]
     for subtable in font.getLookupSubtables(lookup):
@@ -17,11 +19,18 @@ for lookup in font.gsub_lookups + font.gpos_lookups:
                 for possub in glyph.getPosSub(subtable):
                     missing.update([n for n in possub[2:] if n not in font])
 
+failed = len(missing) + len(duplicates)
 with open(sys.argv[2], "w") as logfile:
-    msg = "PASS"
+    if failed == 0:
+        print("PASS", file=logfile)
+
+    if duplicates:
+        msg = "FAIL: Font has duplicate glyphs: %s" % " ".join(duplicates)
+        print(msg)
+        print(msg, file=logfile)
     if missing:
         msg = "Font is missing: %s" % " ".join([n for n in missing])
         print(msg)
-    print(msg, file=logfile)
+        print(msg, file=logfile)
 
-sys.exit(len(missing))
+sys.exit(failed)
