alerque/libertinus

commit bbe6d86d510e118b9b08c76a29ac39aaaa3d7606

Opentype feature synchronization

Makefile
@@ -4,42 +4,59 @@ VERSION=6.7
 DIST=$(NAME)-$(VERSION)
 
 SRC=sources
-FEA=$(SRC)/features
+FEADIR=$(SRC)/features
+GSUB=$(FEADIR)/gsub.fea
 DOC=documentation
 TOOLS=tools
 
 PY?=python
+PREPROP?=preprocess
 BUILD=$(TOOLS)/build.py
+LOADFEAT=$(TOOLS)/load-features.py
 NORMALIZE=$(TOOLS)/sfdnormalize.py
 CHECKERRS=$(TOOLS)/check-errors.py
 LO?=lowriter
 
 NULL=
 
-FONTS=Math-Regular \
-      Sans-Regular \
-      Sans-Bold \
-      Sans-Italic \
-      Serif-Regular \
-      Serif-Semibold \
-      Serif-Bold \
-      Serif-Italic \
-      Serif-SemiboldItalic \
-      Serif-BoldItalic \
-      SerifDisplay-Regular \
-      SerifInitials-Regular \
-      Mono-Regular \
-      Keyboard-Regular \
-      $(NULL)
+MFONTS=Sans-Regular \
+       Sans-Bold \
+       Sans-Italic \
+       Serif-Regular \
+       Serif-Semibold \
+       Serif-Bold \
+       Serif-Italic \
+       Serif-SemiboldItalic \
+       Serif-BoldItalic \
+       SerifDisplay-Regular \
+       $(NULL)
+
+OFONTS=Math-Regular \
+       SerifInitials-Regular \
+       Mono-Regular \
+       Keyboard-Regular \
+       $(NULL)
+
+FONTS=$(MFONTS) \
+      $(OFONTS)
 
 SFD=$(FONTS:%=$(SRC)/$(NAME)%.sfd)
-NRM=$(FONTS:%=$(SRC)/$(NAME)%.nrm)
-CHK=$(FONTS:%=$(SRC)/$(NAME)%.chk)
+MNRM=$(MFONTS:%=$(SRC)/$(NAME)%.nrm)
+ONRM=$(OFONTS:%=$(SRC)/$(NAME)%.nrm)
+NRM=$(MNRM) $(ONRM)
+FEA=$(MFONTS:%=$(SRC)/$(NAME)%.fea)
+MCHK=$(MFONTS:%=$(SRC)/$(NAME)%.chk)
+OCHK=$(OFONTS:%=$(SRC)/$(NAME)%.chk)
+CHK=$(MCHK) $(OCHK)
 DUP=$(FONTS:%=$(SRC)/$(NAME)%.dup)
 LNT=$(FONTS:%=$(NAME)%.lnt)
-OTF=$(FONTS:%=$(NAME)%.otf)
+MOTF=$(MFONTS:%=$(NAME)%.otf)
+OOTF=$(OFONTS:%=$(NAME)%.otf)
+OTF=$(MOTF) $(OOTF)
 PDF=$(FONTS:%=$(DOC)/$(NAME)%-Table.pdf)
 OPDF=$(DOC)/Opentype-Features.pdf $(DOC)/Sample.pdf
+
+FEADEFS=
 
 export SOURCE_DATE_EPOCH ?= 0
 
@@ -47,23 +64,48 @@ all: otf
 
 otf: $(OTF)
 doc: $(PDF) $(OPDF)
+feature-files: $(FEA)
 normalize: $(NRM)
 check: $(LNT) $(CHK) $(DUP)
 
 
-%.fea:
-	@if test ! -f $@; then touch $@; fi
+%.fea: FEADEFS += $(subst Italic,-D ITALIC,$(findstring Italic,$@))
+%.fea: FEADEFS += $(subst Sans,-D SANS,$(findstring Sans,$@))
+%.fea: FEADEFS += $(subst Display,-D NOSMALLCAPS,$(findstring Display,$@))
+%.fea: %.sfd $(GSUB)
+	@echo "   FEA	$@"
+	@$(PREPROP) -c $(FEADIR)/pp_content_types $(FEADEFS) -I $(FEADIR) -o $@ $(GSUB)
 
-%.otf: $(SRC)/%.sfd $(FEA)/%.fea $(BUILD)
+$(MOTF): %.otf: $(SRC)/%.sfd $(SRC)/%.fea $(BUILD)
 	@echo "   OTF	$@"
-	@$(PY) $(BUILD) -o $@ -v $(VERSION) -i $< -f $(FEA)/$(@:%.otf=%.fea)
+	@$(PY) $(BUILD) -f $(SRC)/$*.fea -o $@ -v $(VERSION) -i $<
 
-%.nrm: %.sfd $(NORMALIZE)
+$(OOTF): %.otf: $(SRC)/%.sfd $(BUILD)
+	@echo "   OTF	$@"
+	@$(PY) $(BUILD) -o $@ -v $(VERSION) -i $<
+
+$(MNRM): TMPFILE = $(subst nrm,tmpnrm,$@)
+$(MNRM): %.nrm: %.sfd $(NORMALIZE) $(LOADFEAT)
+	@echo "   NRM	$(<F)"
+	@$(PY) $(LOADFEAT) -e -o $(TMPFILE) $<
+	@$(PY) $(NORMALIZE) $(TMPFILE) $@
+	@rm -f $(TMPFILE)
+	@if [ "`diff -u $< $@`" ]; then cp $@ $<; touch $@; fi
+
+$(ONRM): %.nrm: %.sfd $(NORMALIZE)
 	@echo "   NRM	$(<F)"
 	@$(PY) $(NORMALIZE) $< $@
 	@if [ "`diff -u $< $@`" ]; then cp $@ $<; touch $@; fi
 
-%.chk: %.sfd $(NORMALIZE)
+$(MCHK): TMPFILE = $(subst chk,tmpchk,$@)
+$(MCHK): %.chk: %.sfd $(NORMALIZE) $(LOADFEAT)
+	@echo "   NRM	$(<F)"
+	@$(PY) $(LOADFEAT) -e -o $(TMPFILE) $<
+	@$(PY) $(NORMALIZE) $(TMPFILE) $@
+	@rm -f $(TMPFILE)
+	@diff -u $< $@ || (rm -rf $@ && false)
+
+$(OCHK): %.chk: %.sfd $(NORMALIZE)
 	@echo "   NRM	$(<F)"
 	@$(PY) $(NORMALIZE) $< $@
 	@diff -u $< $@ || (rm -rf $@ && false)
@@ -121,4 +163,7 @@ dist: check $(OTF) $(PDF) $(OPDF)
 	@zip -rq $(DIST).zip $(DIST)
 
 clean:
-	@rm -rf $(DIST) $(DIST).zip $(CHK) $(MIS) $(DUP) $(NRM) $(OTF) $(PDF)
+	@rm -rf $(DIST) $(DIST).zip $(CHK) $(MIS) $(DUP) $(FEA) $(NRM) $(LNT)
+
+cleaner: clean
+	@rm -rf $(MOTF) $(OOTF) $(PDF) $(OPDF)

commit 94de4e6c77ad14012d67795f41976cff39371db4

Unbreak math font
Regression from bbe6d86d510e118b9b08c76a29ac39aaaa3d7606.

Fixes https://github.com/libertinus-fonts/libertinus/issues/267

Makefile
@@ -29,9 +29,10 @@ MFONTS=Sans-Regular \
        Serif-SemiboldItalic \
        Serif-BoldItalic \
        SerifDisplay-Regular \
+       Math-Regular \
        $(NULL)
 
-OFONTS=Math-Regular \
+OFONTS= \
        SerifInitials-Regular \
        Mono-Regular \
        Keyboard-Regular \
@@ -72,6 +73,7 @@ check: $(LNT) $(CHK) $(DUP)
 %.fea: FEADEFS += $(subst Italic,-D ITALIC,$(findstring Italic,$@))
 %.fea: FEADEFS += $(subst Sans,-D SANS,$(findstring Sans,$@))
 %.fea: FEADEFS += $(subst Display,-D NOSMALLCAPS,$(findstring Display,$@))
+%.fea: FEADEFS += $(subst Math,-D MATH,$(findstring Math,$@))
 %.fea: %.sfd $(GSUB)
 	@echo "   FEA	$@"
 	@$(PREPROP) -c $(FEADIR)/pp_content_types $(FEADEFS) -I $(FEADIR) -o $@ $(GSUB)
@@ -82,7 +84,7 @@ $(MOTF): %.otf: $(SRC)/%.sfd $(SRC)/%.fea $(BUILD)
 
 $(OOTF): %.otf: $(SRC)/%.sfd $(BUILD)
 	@echo "   OTF	$@"
-	@$(PY) $(BUILD) -o $@ -v $(VERSION) -i $<
+	@$(PY) $(BUILD) -f $(SRC)/features/$*.fea -o $@ -v $(VERSION) -i $<
 
 $(MNRM): TMPFILE = $(subst nrm,tmpnrm,$@)
 $(MNRM): %.nrm: %.sfd $(NORMALIZE) $(LOADFEAT)

sources/LibertinusMath-Regular.sfd
@@ -43,18 +43,6 @@ OS2CapHeight: 658
 OS2XHeight: 429
 OS2FamilyClass: 261
 OS2Vendor: 'ALIF'
-Lookup: 1 0 0 "'locl' Localized Forms Cyrillic" { "'locl' Localized Forms Cyrillic-1"  } ['locl' ('cyrl' <'SRB ' > ) ]
-Lookup: 6 0 0 "'ccmp' Contextual Chaining Substitution" { "'ccmp' Contextual Chaining Substitution 1"  } ['ccmp' ('DFLT' <'dflt' > 'cyrl' <'dflt' > 'grek' <'dflt' > 'hebr' <'dflt' > 'latn' <'AZE ' 'CRT ' 'TRK ' 'dflt' > ) ]
-Lookup: 1 0 0 "'ss01' Stilgruppe 1" { "'ss01' Stilgruppe 1 1"  } ['ss01' ('DFLT' <'dflt' > 'cyrl' <'dflt' > 'grek' <'dflt' > 'latn' <'AZE ' 'CRT ' 'TRK ' 'dflt' > ) ]
-Lookup: 1 0 0 "'ss02' Stilgruppe 2" { "'ss02' Stilgruppe 2 1"  } ['ss02' ('DFLT' <'dflt' > 'cyrl' <'dflt' > 'grek' <'dflt' > 'latn' <'AZE ' 'CRT ' 'TRK ' 'dflt' > ) ]
-Lookup: 1 0 0 "'ss03' Stilgruppe 3" { "'ss03' Stilgruppe 3 1"  } ['ss03' ('DFLT' <'dflt' > 'cyrl' <'dflt' > 'grek' <'dflt' > 'latn' <'AZE ' 'CRT ' 'TRK ' 'dflt' > ) ]
-Lookup: 1 0 0 "'ss04' Stilgruppe 4" { "'ss04' Stilgruppe 4 1"  } ['ss04' ('DFLT' <'dflt' > 'cyrl' <'dflt' > 'grek' <'dflt' > 'hebr' <'dflt' > 'latn' <'AZE ' 'CRT ' 'TRK ' 'dflt' > ) ]
-Lookup: 1 0 0 "'ss05' Stilgruppe 5" { "'ss05' Stilgruppe 5"  } ['ss05' ('DFLT' <'dflt' > 'cyrl' <'dflt' > 'grek' <'dflt' > 'hebr' <'dflt' > 'latn' <'AZE ' 'CRT ' 'TRK ' 'dflt' > ) ]
-Lookup: 1 0 0 "'ss06' Stilgruppe 6" { "'ss06' Stilgruppe 6-1"  } ['ss06' ('DFLT' <'dflt' > 'cyrl' <'dflt' > 'grek' <'dflt' > 'hebr' <'dflt' > 'latn' <'AZE ' 'CRT ' 'TRK ' 'dflt' > ) ]
-Lookup: 1 0 0 "Substitution dotless forms" { "Substitution dotless forms 1"  } []
-Lookup: 1 0 0 "Substitution cap-Accents" { "Substitution cap-Accents 1"  } []
-Lookup: 1 0 0 "Substitution f short" { "Substitution f short 1"  } []
-Lookup: 6 8 0 "'calt' Contextual Alternates lookup 16" { "'calt' Contextual Alternates lookup 16 subtable"  } ['calt' ('cyrl' <'dflt' > 'grek' <'dflt' > 'latn' <'dflt' 'AZE ' 'CRT ' 'TRK ' > 'DFLT' <'dflt' > ) ]
 Lookup: 258 0 0 "'kern' Cyrillic kerning" { "'kern' Cyrillic Kerning" [150,0,6] } ['kern' ('DFLT' <'dflt' > 'cyrl' <'dflt' > ) ]
 Lookup: 258 0 0 "'kern' Greek kerning" { "'kern' Greek Kerning" [150,0,6] } ['kern' ('DFLT' <'dflt' > 'grek' <'dflt' > ) ]
 Lookup: 260 0 0 "'mark' Markenpositionierung" { "'mark' Top rechts"  "'mark' Komb OR"  "'mark' Top Akzent"  "'mark' Mittelpunkt"  "'mark' Ogonek"  "'mark' Unten Anhang"  "'mark' Unten Punkt"  "'mark' Top Punkt2"  } ['mark' ('DFLT' <'dflt' > 'cyrl' <'dflt' > 'grek' <'dflt' > 'hebr' <'dflt' > 'latn' <'AZE ' 'CRT ' 'TRK ' 'dflt' > ) ]
@@ -544,51 +532,6 @@ KernClass2: 16 15 "'kern' Sans Bold Latin kerning"
  6 u1D5E8
  6 u1D602
  0 {} 0 {} 0 {} 0 {} 0 {} 0 {} 0 {} 0 {} 0 {} 0 {} 0 {} 0 {} 0 {} 0 {} 0 {} 0 {} 98 {} 98 {} 98 {} 24 {} 0 {} 0 {} 0 {} 0 {} 0 {} 0 {} 80 {} 40 {} 0 {} 0 {} 0 {} 55 {} 24 {} 24 {} -5 {} -49 {} -34 {} -49 {} -63 {} -49 {} -20 {} 0 {} 0 {} 37 {} -39 {} 0 {} 24 {} 24 {} 24 {} -37 {} -44 {} -24 {} -61 {} -71 {} -60 {} -40 {} 0 {} 0 {} 24 {} -39 {} 0 {} 24 {} 24 {} 24 {} -34 {} -49 {} -24 {} -112 {} -80 {} -60 {} -40 {} 0 {} 0 {} 24 {} -24 {} 0 {} 0 {} 24 {} 24 {} -24 {} -54 {} 0 {} 0 {} -20 {} -15 {} 0 {} 0 {} 0 {} 24 {} 0 {} 0 {} -5 {} -34 {} 37 {} 0 {} 10 {} 10 {} -29 {} 0 {} 0 {} 0 {} 0 {} 0 {} 0 {} 0 {} 0 {} -39 {} -12 {} -24 {} 10 {} 15 {} 10 {} -49 {} -8 {} 0 {} 0 {} 0 {} 0 {} 0 {} 0 {} 0 {} 0 {} -12 {} -24 {} 0 {} 0 {} 10 {} 0 {} 0 {} 0 {} 0 {} 0 {} 0 {} 0 {} 0 {} 0 {} -29 {} -37 {} -44 {} 0 {} 0 {} 0 {} -20 {} 10 {} 0 {} 0 {} 0 {} 0 {} 0 {} 0 {} 0 {} -49 {} -54 {} -54 {} 0 {} -7 {} -7 {} -40 {} 7 {} 0 {} 0 {} 0 {} 0 {} 0 {} 0 {} 0 {} 0 {} -49 {} 0 {} 0 {} 0 {} 0 {} 0 {} 0 {} 0 {} 0 {} 0 {} 0 {} 0 {} 0 {} 0 {} -63 {} -73 {} -112 {} -34 {} -65 {} 0 {} 0 {} -12 {} -12 {} 0 {} 0 {} 0 {} 0 {} -34 {} 0 {} 37 {} 24 {} 24 {} 0 {} 0 {} 0 {} -59 {} 0 {} 0 {} 0 {} 0 {} 0 {} 24 {} 0 {} 0 {} 0 {} 24 {} 24 {} 0 {} 0 {} 0 {} 0 {} 0 {} 0 {} 0 {} 0 {} 0 {} 24 {} 0 {} 0 {} 0 {} 0 {} 24 {} 0 {} 0 {} 0 {} -59 {} 0 {} 0 {} 0 {} 0 {} 12 {} 49 {} 0 {}
-ChainSub2: coverage "'calt' Contextual Alternates lookup 16 subtable" 0 0 0 1
- 1 0 1
-  Coverage: 5 f f_f
-  FCoverage: 411 b h i j k l igrave iacute icircumflex idieresis thorn hcircumflex hbar itilde imacron ibreve iogonek ij jcircumflex lacute uni013C lcaron ldot lslash uni0180 uni0185 uni0196 uni019A uni01D0 uni01F0 uni0209 uni020B uni021F uni0234 uni0249 uni1E03 uni1E05 uni1E07 uni1E23 uni1E25 uni1E27 uni1E29 uni1E2B uni1E2D uni1E2F uni1E31 uni1E33 uni1E35 uni1E37 uni1E39 uni1E3B uni1E3D uni1EC9 uni1ECB uni1EFB uni2C61 h.alt
- 1
-  SeqLookup: 0 "Substitution f short"
-EndFPST
-ChainSub2: class "'ccmp' Contextual Chaining Substitution 1" 6 6 6 3
-  Class: 11 i j uni0268
-  Class: 398 gravecomb acutecomb uni0302 tildecomb uni0304 uni0305 uni0306 uni0307 uni0308 hookabovecomb uni030A uni030B uni030C uni030D uni030E uni030F uni0310 uni0311 uni0312 uni0313 uni0314 uni0315 uni031A uni0351 uni0357 uni0358 grave.cap acute.cap circumflex.cap caron.cap breve.cap hungarumlaut.cap space_uni030F.cap breveinvertedcmb.cap breve.cyrcap breve.cyr dieresis.cap hookabovecomb.cap dotaccent.cap
-  Class: 5 f f_f
-  Class: 376 parenright question T V W Y bracketright bar braceright brokenbar ordfeminine uni00B2 uni00B3 paragraph uni00B9 Yacute agrave atilde adieresis egrave edieresis igrave idieresis ograve otilde odieresis ugrave udieresis ydieresis amacron abreve emacron ebreve ecaron itilde imacron ibreve omacron umacron ubreve quoteleft quoteright quotedblleft quotedblright uni2047 uni2048 ij
-  Class: 536 A B C D E F G H I J K L M N O P Q R S U X Z Acircumflex AE Ccedilla Ocircumflex Thorn Alpha Beta Gamma uni0394 Epsilon Zeta Eta Theta Iota Kappa Lambda Mu Nu Xi Omicron Pi Rho NameMe.930 Sigma Tau Upsilon Phi Chi Psi uni03A9 uni0404 uni0405 uni0406 uni0408 uni0409 uni040A uni040B uni040F uni0410 uni0411 uni0412 uni0413 uni0414 uni0415 uni0416 uni0417 uni0418 uni041A uni041B uni041C uni041D uni041E uni041F uni0420 uni0421 uni0422 uni0423 uni0424 uni0425 uni0426 uni0427 uni0428 uni0429 uni042A uni042B uni042C uni042D uni042E uni042F
-  BClass: 11 i j uni0268
-  BClass: 398 gravecomb acutecomb uni0302 tildecomb uni0304 uni0305 uni0306 uni0307 uni0308 hookabovecomb uni030A uni030B uni030C uni030D uni030E uni030F uni0310 uni0311 uni0312 uni0313 uni0314 uni0315 uni031A uni0351 uni0357 uni0358 grave.cap acute.cap circumflex.cap caron.cap breve.cap hungarumlaut.cap space_uni030F.cap breveinvertedcmb.cap breve.cyrcap breve.cyr dieresis.cap hookabovecomb.cap dotaccent.cap
-  BClass: 5 f f_f
-  BClass: 376 parenright question T V W Y bracketright bar braceright brokenbar ordfeminine uni00B2 uni00B3 paragraph uni00B9 Yacute agrave atilde adieresis egrave edieresis igrave idieresis ograve otilde odieresis ugrave udieresis ydieresis amacron abreve emacron ebreve ecaron itilde imacron ibreve omacron umacron ubreve quoteleft quoteright quotedblleft quotedblright uni2047 uni2048 ij
-  BClass: 536 A B C D E F G H I J K L M N O P Q R S U X Z Acircumflex AE Ccedilla Ocircumflex Thorn Alpha Beta Gamma uni0394 Epsilon Zeta Eta Theta Iota Kappa Lambda Mu Nu Xi Omicron Pi Rho NameMe.930 Sigma Tau Upsilon Phi Chi Psi uni03A9 uni0404 uni0405 uni0406 uni0408 uni0409 uni040A uni040B uni040F uni0410 uni0411 uni0412 uni0413 uni0414 uni0415 uni0416 uni0417 uni0418 uni041A uni041B uni041C uni041D uni041E uni041F uni0420 uni0421 uni0422 uni0423 uni0424 uni0425 uni0426 uni0427 uni0428 uni0429 uni042A uni042B uni042C uni042D uni042E uni042F
-  FClass: 11 i j uni0268
-  FClass: 398 gravecomb acutecomb uni0302 tildecomb uni0304 uni0305 uni0306 uni0307 uni0308 hookabovecomb uni030A uni030B uni030C uni030D uni030E uni030F uni0310 uni0311 uni0312 uni0313 uni0314 uni0315 uni031A uni0351 uni0357 uni0358 grave.cap acute.cap circumflex.cap caron.cap breve.cap hungarumlaut.cap space_uni030F.cap breveinvertedcmb.cap breve.cyrcap breve.cyr dieresis.cap hookabovecomb.cap dotaccent.cap
-  FClass: 5 f f_f
-  FClass: 376 parenright question T V W Y bracketright bar braceright brokenbar ordfeminine uni00B2 uni00B3 paragraph uni00B9 Yacute agrave atilde adieresis egrave edieresis igrave idieresis ograve otilde odieresis ugrave udieresis ydieresis amacron abreve emacron ebreve ecaron itilde imacron ibreve omacron umacron ubreve quoteleft quoteright quotedblleft quotedblright uni2047 uni2048 ij
-  FClass: 536 A B C D E F G H I J K L M N O P Q R S U X Z Acircumflex AE Ccedilla Ocircumflex Thorn Alpha Beta Gamma uni0394 Epsilon Zeta Eta Theta Iota Kappa Lambda Mu Nu Xi Omicron Pi Rho NameMe.930 Sigma Tau Upsilon Phi Chi Psi uni03A9 uni0404 uni0405 uni0406 uni0408 uni0409 uni040A uni040B uni040F uni0410 uni0411 uni0412 uni0413 uni0414 uni0415 uni0416 uni0417 uni0418 uni041A uni041B uni041C uni041D uni041E uni041F uni0420 uni0421 uni0422 uni0423 uni0424 uni0425 uni0426 uni0427 uni0428 uni0429 uni042A uni042B uni042C uni042D uni042E uni042F
- 1 0 1
-  ClsList: 1
-  BClsList:
-  FClsList: 2
- 1
-  SeqLookup: 0 "Substitution dotless forms"
- 1 0 1
-  ClsList: 3
-  BClsList:
-  FClsList: 4
- 1
-  SeqLookup: 0 "Substitution f short"
- 1 1 0
-  ClsList: 2
-  BClsList: 5
-  FClsList:
- 1
-  SeqLookup: 0 "Substitution cap-Accents"
-  ClassNames: "0" "1" "2" "3" "4" "5"
-  BClassNames: "0" "1" "2" "3" "4" "5"
-  FClassNames: "0" "1" "2" "3" "4" "5"
-EndFPST
 LangName: 1033 "" "" "" "" "" "" "" "" "Khaled Hosny" "Philipp H. Poll" "" "https://github.com/libertinus-fonts/libertinus" "http://www.linuxlibertine.org" "This Font Software is licensed under the SIL Open Font License, Version 1.1." "https://scripts.sil.org/OFL"
 GaspTable: 3 8 2 17 1 65535 3 0
 MATH:ScriptPercentScaleDown: 80
@@ -11126,7 +11069,6 @@ SplineSet
  300 614 251 585 251 519 c 0
  251 496 263 458 289 409 c 1
 EndSplineSet
-Substitution2: "'ss06' Stilgruppe 6-1" ampersand.alt
 EndChar
 
 StartChar: quotesingle
@@ -11276,7 +11218,6 @@ SplineSet
  389 641 389 620 383 614 c 1
  323 611 306 606 306 523 c 2
 EndSplineSet
-Substitution2: "'ss02' Stilgruppe 2 1" J.alt
 EndChar
 
 StartChar: K
@@ -11328,7 +11269,6 @@ SplineSet
  253.7 296.6 224.8 307.9 191 308.9 c 1
  191 122 l 2
 EndSplineSet
-Substitution2: "'ss02' Stilgruppe 2 1" K.alt
 Colour: ff9ccc
 EndChar
 
@@ -11607,7 +11547,6 @@ SplineSet
  291.7 276.3 273 291 189 291 c 1
  189 122 l 2
 EndSplineSet
-Substitution2: "'ss02' Stilgruppe 2 1" R.alt
 EndChar
 
 StartChar: S
@@ -11808,7 +11747,6 @@ SplineSet
  653.7 130.7 l 1
  810 565 l 2
 EndSplineSet
-Substitution2: "'ss05' Stilgruppe 5" W.alt
 EndChar
 
 StartChar: X
@@ -12277,7 +12215,6 @@ SplineSet
  171.5 603.5 175 553 175 486 c 2
  175 429 l 1
 EndSplineSet
-Substitution2: "Substitution f short 1" f.short
 EndChar
 
 StartChar: g
@@ -12412,7 +12349,6 @@ SplineSet
  185 435 181 371 181 321.3 c 2
  181 122 l 2
 EndSplineSet
-Substitution2: "Substitution dotless forms 1" dotlessi
 Colour: ff00ff
 EndChar
 
@@ -12447,7 +12383,6 @@ SplineSet
  169 -179 173 -56 173 76 c 2
  173 317 l 2
 EndSplineSet
-Substitution2: "Substitution dotless forms 1" uni0237
 Colour: ff00ff
 EndChar
 
@@ -15517,7 +15452,6 @@ SplineSet
  17 4 17 25 23 31 c 1
  80 34 92 39 92 122 c 2
 EndSplineSet
-Substitution2: "'ss03' Stilgruppe 3 1" germandbls.ss03
 EndChar
 
 StartChar: agrave
@@ -27843,7 +27777,6 @@ SplineSet
  -228 693 -205 710 -171 710 c 0
  -166 710 -160 709 -156 708 c 1
 EndSplineSet
-Substitution2: "Substitution cap-Accents 1" grave.cap
 EndChar
 
 StartChar: acutecomb
@@ -27868,7 +27801,6 @@ SplineSet
  -239 572.2 -235.7 576.2 -227.6 589 c 2
  -152 708 l 1
 EndSplineSet
-Substitution2: "Substitution cap-Accents 1" acute.cap
 EndChar
 
 StartChar: uni0302
@@ -27892,7 +27824,6 @@ SplineSet
  -230 611 -184 665 -155 711 c 1
  -144.3 713.3 -133.7 714 -123 711 c 1
 EndSplineSet
-Substitution2: "Substitution cap-Accents 1" circumflex.cap
 EndChar
 
 StartChar: tildecomb
@@ -31548,7 +31479,6 @@ SplineSet
  456 92 377.3 -10 240 -10 c 0
  143 -10 34 67.7 34 257 c 0
 EndSplineSet
-Substitution2: "'locl' Localized Forms Cyrillic-1" uni0431.ital
 EndChar
 
 StartChar: uni0432
@@ -49893,7 +49823,6 @@ SplineSet
  368 488.7 369 528 378 564 c 1
 EndSplineSet
 LCarets2: 1 266
-Substitution2: "Substitution f short 1" f_f.short
 EndChar
 
 StartChar: f_i
@@ -55813,7 +55742,6 @@ SplineSet
  -231 529 -282 579 -296 664 c 1
  -269 676 l 1
 EndSplineSet
-Substitution2: "Substitution cap-Accents 1" breve.cap
 EndChar
 
 StartChar: uni0304
@@ -64156,7 +64084,6 @@ SplineSet
  221 241 215 237 210 224 c 2
  155 79 l 2
 EndSplineSet
-Substitution2: "'ss01' Stilgruppe 1 1" Adieresis
 Comment: "Dies sind alternative Umlaute, die im Deutschen vorzuziehen sind."
 Colour: ff00ff
 EndChar
@@ -64190,7 +64117,6 @@ SplineSet
  37 503.9 150.3 658 344 658 c 0
  523 658 666 536 666 329 c 0
 EndSplineSet
-Substitution2: "'ss01' Stilgruppe 1 1" Odieresis
 Comment: "Dies sind alternative Umlaute, die im Deutschen vorzuziehen sind."
 Colour: ff00ff
 EndChar
@@ -64235,7 +64161,6 @@ SplineSet
  270 641 270 620 264 614 c 1
  187 611 172 606 172 523 c 2
 EndSplineSet
-Substitution2: "'ss01' Stilgruppe 1 1" Udieresis
 Comment: "Dies sind alternative Umlaute, die im Deutschen vorzuziehen sind."
 Colour: ff00ff
 EndChar
@@ -64794,7 +64719,6 @@ SplineSet
  195 230 l 1
  195 122 l 2
 EndSplineSet
-Substitution2: "Substitution dotless forms 1" dotlessi
 Colour: ff9ccc
 EndChar
 
@@ -85595,8 +85519,6 @@ SplineSet
  287 621 189 529 189 320 c 2
  189 122 l 2
 EndSplineSet
-Substitution2: "'ss03' Stilgruppe 3 1" uni1E9E.alt
-Substitution2: "'ss04' Stilgruppe 4 1" uni1E9E.alt
 EndChar
 
 StartChar: uni2201

sources/features/gsub.fea
@@ -1,3 +1,7 @@
+# #ifdef MATH
+# #define NOSMALLCAPS
+# #endif
+
 languagesystem DFLT dflt;
 languagesystem cyrl dflt;
 languagesystem cyrl SRB;
@@ -15,7 +19,11 @@ languagesystem latn SKS;
 languagesystem latn SSM;
 languagesystem latn SVE;
 languagesystem latn TRK;
+# #ifdef MATH
+languagesystem math dflt;
+# #endif
 
+# #ifndef MATH
 # #include "locl.fea"
 
 # #include "frac.fea"
@@ -23,6 +31,7 @@ languagesystem latn TRK;
 # #include "small.fea"
 
 # #include "case.fea"
+# #endif
 
 # #ifndef NOSMALLCAPS
 # #include "sc.fea"
@@ -32,11 +41,13 @@ languagesystem latn TRK;
 
 # #include "num.fea"
 
+# #ifndef MATH
 # #include "salt.fea"
 
 # #include "ccmp.fea"
 
 # #include "sscv.fea"
+# #endif
 
 # #ifdef SANS
 # #include "liga_sans.fea"
@@ -44,4 +55,10 @@ languagesystem latn TRK;
 # #include "liga.fea"
 # #endif
 
+# #ifndef MATH
 # #include "calt.fea"
+# #endif
+
+# #ifdef MATH
+# #include "ssty.fea"
+# #endif

sources/features/num.fea
@@ -1,3 +1,4 @@
+# #ifndef MATH
 feature onum {
   sub zero by zero.taboldstyle;
   sub one by one.taboldstyle;
@@ -93,10 +94,13 @@ feature tnum {
   sub Euro.fitted by Euro;
   sub Yen.fitted by yen;
 } tnum;
+# #endif
 
 feature zero {
   sub zero by zero.slash;
+# #ifndef MATH
   sub zero.fitted by zero.slash.fitted;
   sub zero.cap by zero.slash.cap;
   sub zero.cap.fitted by zero.slash.cap.fitted;
+# #endif
 } zero;

sources/features/ssty.fea
@@ -1,0 +1,8 @@
+feature ssty {
+  sub minute by minute.ssty1;
+  sub second by second.ssty1;
+  sub uni2034 by uni2034.ssty1;
+  sub uni2035 by uni2035.ssty1;
+  sub uni2036 by uni2036.ssty1;
+  sub uni2037 by uni2037.ssty1;
+} ssty;